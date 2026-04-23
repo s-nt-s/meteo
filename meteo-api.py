86128e7b6730ff98d4ca5d7e3bb3f8648de3a0cd
s-nt-s <santos82@gmail.com>
2026-04-23 09:29:30 +0200

    esquema

--- meteo-api.py
+++ meteo-api.py
@@ -1,28 +1,50 @@
 from os import environ, chdir, makedirs
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import json
 from os.path import dirname, abspath, isfile, getmtime
 from time import time
 import re
 from typing import NamedTuple
+from time import sleep
 
 
 chdir(dirname(abspath(__file__)))
 
 
+class MinMax(NamedTuple):
+    min: int
+    max: int
+
+
 class Data(NamedTuple):
-    estadoCielo: str #
+    # Diaria/Horaria: Descripción del estado del cielo
+    estadoCielo: str
+    # Horaria: Milímetros (mm) de precipitación durante la hora anterior
     precipitacion: int
-    probPrecipitacion: int #
+    # Diaria/Horaria: % de probabilidad de precipitación
+    probPrecipitacion: int
+    # Horaria: % de probabilidad de tormenta
     probTormenta: int
+    # Horaria: milímetros (mm) de nieve que se prevé que caiga durante la hora anterior
     nieve: int
+    # Horaria: % de probabilidad de precipitación de nieve
     probNieve: int
-    temperatura: int #
-    sensTermica: int #
-    humedadRelativa: int #
-    viento: int #
+    # Horaria: Grados celsius
+    # Diaria: [Min-Max] Grados celsius
+    temperatura: MinMax
+    # Horaria: Grados celsius
+    # Diaria: [Min-Max] Grados celsius
+    sensTermica: MinMax
+    # Horaria: % de humedad relativa
+    # Diaria: [Min-Max] % de humedad relativa
+    humedadRelativa: MinMax
+    # Diaria/Horaria: km/h viento
+    viento: int
+    # Diaria/Horaria: km/h racha máxima
     rachaMax: int
-    uvMax: int ##
+    # Diaria: Índice ultravioleta máximo
+    uvMax: int
 
 
 def load_env(path=".env"):
@@ -81,6 +103,8 @@ def _parse(obj, key: str = None):
         obj = obj.strip()
         if len(obj) == 0:
             return None
+        if key in ("elaborado", "fecha"):
+            return obj.replace("T", " ")[:16 if key == "elaborado" else 10]
         return _to_num(obj)
     return obj
 
@@ -109,14 +133,24 @@ def _provicina(nombre: str, provincia: str):
 class Meteo:
     def __init__(self, id: str, minutes_cache: int = 30):
         self.__root = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio"
-        self.__api_key = environ['AEMET_KEY']
+        self.__api_key = tuple(sorted(set(environ['AEMET_KEY'].strip().split())))
         self.__seconds_cache = minutes_cache * 60
         self.__id = id
 
-    def __get_json(self, url: str):
-        r = _get_json(url)
-        d = _get_json(r['datos'])
-        return d
+    def __get_json(self, path: str):
+        prefix = f"{self.__root}{path}/{self.__id}"
+        for api_key in self.__api_key:
+            max_tries = 3
+            for i in range(0, max_tries + 1):
+                try:
+                    url = f"{prefix}?api_key={api_key}"
+                    r = _get_json(url)
+                    d = _get_json(r['datos'])
+                    return d
+                except HTTPError as e:
+                    if i==max_tries or not re.search("Too Many Requests", str(e), flags=re.I):
+                        raise
+                sleep(2*(i+1))
 
     def __get_data(self, path: str) -> dict:
         file = f"cache/{self.__id}/{path}.json"
@@ -124,8 +158,7 @@ class Meteo:
             with open(file, "r") as f:
                 return json.load(f)
 
-        url = f"{self.__root}{path}/{self.__id}?api_key={self.__api_key}"
-        data = self.__get_json(url)
+        data = self.__get_json(path)
         data = _parse(data)
         if not isinstance(data, list) or len(data) != 1 or not isinstance(data[0], dict):
             raise ValueError(data)
@@ -133,7 +166,7 @@ class Meteo:
 
         data = {
             "id": data['id'],
-            "elaborado": data['elaborado'].replace("T", " ")[:16],
+            "elaborado": data['elaborado'],
             "provincia": _provicina(data['nombre'], data['provincia']),
             **data['prediccion']
         }
